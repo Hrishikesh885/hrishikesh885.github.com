580e3d66545662c1a0447b08364212c764ab06e9
Hrishikesh885 <kavale.hrishikesh@gmail.com>
2020-06-20 17:49:15 +0530

    best nav bar

---
+++ hide.js
@@ -0,0 +1,20 @@
+//for basic
+function show_hide(){
+	var click=document.getElementById("basic");
+	if(click.style.display ==="none"){
+	click.style.display ="block";
+	
+	}else{
+	click.style.display = "none"
+	}
+}
+//for theory
+function showhide(){
+	var click=document.getElementById("theory");
+	if(click.style.display ==="none"){
+	click.style.display ="block";
+	
+	}else{
+	click.style.display = "none"
+	}
+}
